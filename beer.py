cfc15a2ace447c7d97fdb640468f1f7abb5ee23a
Jamison Griffith <jamison@zingstudios.com>
2023-04-19 09:40:26 -0600

    Add TODOs, Readme

---
+++ beer.py
@@ -0,0 +1,6 @@
+#TODO: Beer class
+
+# Write init function to create an instance of Beer class.
+# Pass in id, name, type, manufacturer and ibu as arguments to init
+
+# TODO: write instance method called 'isHoppy()' that returns true/false if Ibu's greater than 100
--- beer_dicts.py
+++ beer_dicts.py
@@ -1,28 +1,32 @@
-
+#TODO: refactor these dicts into instances of Beer class
 Coors = {
+    'Id': 1,
     'Name'          : 'Coors',
     'Type'          : 'Lager',
     'Manufacturer'  : 'Molson Coors',
-    'Ibu'           : '15',
+    'Ibu'           : 15,
 }
 
 Avery = {
+    'Id': 2,
     'Name'          : 'Avery IPA',
     'Type'          : 'American IPA',
     'Manufacturer'  : 'Avery Brewing Company',
-    'Ibu'           : '69',
+    'Ibu'           : 69,
 }
 
 Jamison = {
+    'Id': 3,
     'Name'          : 'Jamison IPA',
     'Type'          : 'Rare IPA',
     'Manufacturer'  : 'GriffithWorks',
-    'Ibu'           : '1000000',
+    'Ibu'           : 1000000,
 }
 
 David = {
+    'Id': 4,
     'Name'          : 'David IPA',
     'Type'          : 'Rare IPA',
     'Manufacturer'  : 'BeerWorld by David',
-    'Ibu'           : '1000000',
+    'Ibu'           : 1000000,
 }
