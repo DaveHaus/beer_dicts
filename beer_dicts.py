316088f9c61ffbc6b29868ac844e0a4602cfeaa3
DaveHaus <djhausman@gmail.com>
2023-04-19 08:38:04 -0600

    Add files via upload

---
+++ beer_dicts.py
@@ -0,0 +1,28 @@
+
+Coors = {
+    'Name'          : 'Coors',
+    'Type'          : 'Lager',
+    'Manufacturer'  : 'Molson Coors',
+    'Ibu'           : '15',
+}
+
+Avery = {
+    'Name'          : 'Avery IPA',
+    'Type'          : 'American IPA',
+    'Manufacturer'  : 'Avery Brewing Company',
+    'Ibu'           : '69',
+}
+
+Jamison = {
+    'Name'          : 'Jamison IPA',
+    'Type'          : 'Rare IPA',
+    'Manufacturer'  : 'GriffithWorks',
+    'Ibu'           : '1000000',
+}
+
+David = {
+    'Name'          : 'David IPA',
+    'Type'          : 'Rare IPA',
+    'Manufacturer'  : 'BeerWorld by David',
+    'Ibu'           : '1000000',
+}
